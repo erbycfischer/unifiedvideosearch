1fd83357c86e0d3bdda12b7132c6ed13ffb0481b
Braden Groom <braden.groom@gmail.com>
2013-11-02 15:42:02 -0500

    The HuluParser.py uses lxml now rather than regex. The object oriented part does not work yet, but the seach function was tested by itself and seemsto work. I don't think we should store videos in a dictionary since we could have multiple videos with the same name

--- src/parsers/HuluParser.py
+++ src/parsers/HuluParser.py
@@ -1,12 +1,47 @@
-import urllib.request
+import Parser
+import urllib
 import http.cookiejar
+from lxml import html
 import re
 
 class HuluParser(Parser):
 
-	def get_videos(self):
-		req = urllib.request.Request("https://www.hulu.com/browse/movies", headers={'User Agent' : "Magic Browser"})
-		html = str(urllib.request.urlopen(req).read())
+	def get_videos():
 
-		results = re.findall(r'<a href[^<]*?\(this\);">(.*?)</a>',html)
-		return results
+		videos = []
+
+		req = urllib.request.Request("https://www.hulu.com/browse/tv", headers={'User Agent' : "Magic Browser"})
+		source = urllib.request.urlopen(req).read()
+
+		doc = html.fromstring(source)
+
+		for el in doc.get_element_by_id("show_list_hiden"):
+			tag = el.find_class("beaconid")[0]
+			title = tag.text
+			link = tag.get("href")
+			movieID = tag.get("beaconid")
+
+			videos.append((title, [movieID, link]))
+
+		req = urllib.request.Request("https://www.hulu.com/browse/movies", headers={'User Agent' : "Magic Browser"})
+		source = urllib.request.urlopen(req).read()
+
+		doc = html.fromstring(source)
+
+		for el in doc.get_element_by_id("show_list_hiden"):
+			tag = el.find_class("beaconid")[0]
+			title = tag.text
+			link = tag.get("href")
+			movieID = tag.get("beaconid")
+
+			videos.append((title, [movieID, link]))
+
+		return videos
+
+	def search(query):
+		videos = get_videos()
+		results = []
+
+		for video in videos:
+			if (re.search(query, video[0], re.IGNORECASE)):
+				results.append(video)

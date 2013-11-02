96908a9fb23654c5be96a94dbede0b599a33bd83
Braden Groom <braden.groom@gmail.com>
2013-11-02 15:01:44 -0500

    fixed indentation error in Parser.py.  The NetflixParser.py also uses lxml now rather than regex.  The search function now takes in a search with spaces and replaces them with pluses for you.  The object oriented part does not work yet, but the seach function was tested by itself and seemsto work.

--- src/parsers/NetflixParser.py
+++ src/parsers/NetflixParser.py
@@ -1,26 +1,34 @@
-import urllib.request
+import Parser
+import urllib
 import http.cookiejar
-import re
+from lxml import html
 
 class NetflixParser(Parser):
 
 	def search(self, movie):
+
+		movie =  movie.strip()
+		movie = movie.replace(" ", "+")
+
 		jar = http.cookiejar.CookieJar()
 		opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(jar))
 
-		html = str(opener.open('https://signup.netflix.com/Search?v1='+movie).read())
+		source = opener.open('https://signup.netflix.com/Search?v1='+movie).read()
 
-		results = re.findall(r'<section class="movie-result agMovie">.*?</section>',html)
+		doc = html.fromstring(source)
 
 		videos = []
-		for result in results:
-			title = re.search(r'alt="(.*?)"',result).group(1)
-			movieID = re.search(r'data-movieid="(.*?)"',result).group(1)
-			link = re.search(r'href="(.*?)"',result).group(1)
-			image = re.search(r'src="(.*?)"',result).group(1)
-			time = re.search(r'<time class="mdp-date">(.*?)</time>',result).group(1)
-			span = re.search(r'<span class="mdp-season">(.*?)</span>',result)
+		for el in doc.get_element_by_id("search-resultlist"):
+			tag = el.find_class("mdpLink")[1]
+			title = tag.text
+			movieID = tag.get("data-movieid")
+			link = tag.get("href")
+			time = el.find_class("mdp-date")[0].text
+			spantag = el.find_class("mdp-season")
+			span = None
+			if len(spantag):
+				span = spantag[0].text
 
-			videos.append({title: [movieID, link, image, time, span.group(1)]})
+			videos.append({title: [movieID, link, time, span]})
 
 		return videos

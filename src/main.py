9c66a11b2b0f8c528e8aeeb0ce9ec74b1380dc05
Erby Fischer <erbycfischer@mac.com>
2013-11-02 19:14:41 -0500

    Updated .gitignore, added redis.init and a config parser

--- src/main.py
+++ src/main.py
@@ -1,11 +1,28 @@
 import bottle
 from bottle import route, run, template
 
-@route('/hello/<name>')
-def index(name='World'):
-	return template('<b>Hello {{name}}</b>!', name=name)
+import configparser
+
+@route('/')
+def index():
+	return template('''
+			<html>
+			<head><title>Test</title></head>
+			<body>
+			<div align=\'center\'>
+			<h2>Test site!</h2>
+			</div>
+			</body>
+			</html>
+			''')
 
 if __name__ == "__main__":
 	run(host='localhost', port=8080)
+
+config = configparser.ConfigParser()
+config.read('../redis.ini')
+
+#redis_host = config['redis']['host']
+#redis_port = config['redis']['port']
 
 app = bottle.default_app()
